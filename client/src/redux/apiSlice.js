402dbbbadd89c777aa38bbb503c0dbf10db9d257
kavinesh <kavineshduraisamy@gmail.com>
2026-02-25 23:14:27 +0530

    fixing cors while deploying vercel

--- client/src/redux/apiSlice.js
+++ client/src/redux/apiSlice.js
@@ -3,7 +3,11 @@ import { BASE_URL } from '../constants';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL,
-    prepareHeaders: (headers) => {
+    prepareHeaders: (headers, { getState }) => {
+        const token = getState().auth.userInfo?.token;
+        if (token) {
+            headers.set('authorization', `Bearer ${token}`);
+        }
         return headers;
     },
     credentials: 'include',
